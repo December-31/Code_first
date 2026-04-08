91a3aba0a66174672c331fe8f2b372ca9b08e8e8
December-31 <vancuong31122k6@gmail.com>
2026-04-08 15:57:53 +0700

    Tạo thêm thư mục Data và file context

---
+++ Code_first/Models/Data/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Code_first.Models.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Classroom> Classrooms { get; set; }
+    }
+}
