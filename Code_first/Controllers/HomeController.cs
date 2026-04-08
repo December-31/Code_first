4a1631dc0a10805480f0c877fc70abfae67f7a50
December-31 <vancuong31122k6@gmail.com>
2026-04-08 15:45:46 +0700

    Add project files.

---
+++ Code_first/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Code_first.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Code_first.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
