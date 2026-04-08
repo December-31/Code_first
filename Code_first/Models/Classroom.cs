03761df0e0fe4da59bf65cde38a720079a74a5eb
December-31 <vancuong31122k6@gmail.com>
2026-04-08 15:53:30 +0700

    Tạo 2 lớp Student và Classroom

---
+++ Code_first/Models/Classroom.cs
@@ -0,0 +1,9 @@
+﻿namespace Code_first.Models
+{
+    public class Classroom
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; }
+        public virtual ICollection<Student> Students { get; set; } 
+    }
+}
---
+++ Code_first/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_first.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ClassroomId { get; set; } 
+        public virtual Classroom Classroom { get; set; }
+    }
+}
